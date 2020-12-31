c4acc8a6c4547d8478372d723f955a5c8f9ca43d
hitozhang <hitozhang@tencent.com>
2020-12-31 18:29:03 +0800

    重构: 替换为默认主题

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  title: 'hito-blog',
+  description: 'hito-blog',
+  dest: 'dist',
+  head: [
+      ['link', { rel: 'icon', href: `/favicon.png` }],
+  ],
+  plugins: [
+      ['redirect', {
+          redirectors: [
+              { base: '/frontend', alternative: ['canvas/事件'] }
+          ]
+      }]
+  ],
+  themeConfig: {
+      logo: '/favicon.png',
+      lastUpdated: '上次更新',
+      editLinks: true,
+      docsDir: 'docs',
+      nav: [
+          { text: '前端', link: '/frontend/' },
+          // { text: '后端', link: '/backend/' },
+          // { text: 'Demo', link: '/samples/' },
+      ],
+      sidebar: {
+          '/frontend/': [
+              {
+                  title: 'Canvas',
+                  collapsable: false,
+                  children: [
+                      'canvas/事件'
+                  ]
+              }
+          ]
+      }
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /hero.png
+actionText: 开始 →
+actionLink: /frontend/
+footer: MIT Licensed | Copyright © 2020 Chart.js Contributors
+features:
+- title: 前端
+  details: 前端问题记录
+- title: 后端
+  details: 后端问题记录
+- title: Demo
+  details: 例子
+---
